69a272636d8c69c11ef255edfd3d176b4aecc9ab
Bruno Félix <ispy02@gmail.com>
2023-04-25 09:58:06 +0100

    Add Docker+Kubernetes files

--- Program.cs
+++ Program.cs
@@ -42,9 +42,8 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseHttpsRedirection();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
